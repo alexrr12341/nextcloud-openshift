99fcd75b7a6407aab85016dbac1c266ec419992f
alexrr12341 <alexrodriguezrojas98@gmail.com>
2020-04-24 13:38:07 +0200

    nextcloud

---
+++ version.php
@@ -0,0 +1,17 @@
+<?php 
+$OC_Version = array(18,0,4,2);
+$OC_VersionString = '18.0.4';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = array (
+  'nextcloud' => 
+  array (
+    '17.0' => true,
+    '18.0' => true,
+  ),
+  'owncloud' => 
+  array (
+  ),
+);
+$OC_Build = '2020-04-22T19:26:57+00:00 1a1848c402bac29a40554fe74d39568c95131db9';
+$vendor = 'nextcloud';
